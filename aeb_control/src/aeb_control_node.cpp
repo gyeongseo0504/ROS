787eceee383ae2502d04366939bc83a6de1f3000
gyeongseo0504 <125629732+gyeongseo0504@users.noreply.github.com>
2024-01-17 11:25:10 +0900

    Add files via upload

--- aeb_control/src/aeb_control_node.cpp
+++ aeb_control/src/aeb_control_node.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include "std_msgs/Int8.h"
 #include "std_msgs/Float64.h"
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/Range.h"
@@ -7,48 +8,39 @@ double front_range = 0.0;
 
 void Sonar_FrontCallback(const sensor_msgs::Range::ConstPtr& msg)
 {
-    front_range = msg->range;
-    
-    if (front_range <= 2.0)
-    {
-        ROS_INFO("Sonar Range: [%f] - Stopping", front_range);
-    }
-    else
-    {
-        ROS_INFO("Sonar Range: [%f]", front_range);
-    }
+   front_range = msg->range;   
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    int count = 0;
+  int count = 0;
+  
+  ros::init(argc, argv, "AEB_system");
+  geometry_msgs::Twist msg_cmd;  
+  
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("/range_front", 1000, Sonar_FrontCallback);
+  ros::Publisher pub_cmd_vel = n.advertise<geometry_msgs::Twist>("/ackermann_steering_controller/cmd_vel", 1000);
 
-    ros::init(argc, argv, "AEB_system");
-    ros::NodeHandle n;
-    
-    geometry_msgs::Twist msgs_cmd;
-    ros::Publisher pub_cmd_vel = n.advertise<geometry_msgs::Twist>("/ackermann_steering_controller/cmd_vel", 1000);
-    ros::Subscriber sub = n.subscribe("sensor/sonar0", 1000, Sonar_FrontCallback);
-
-    ros::Rate loop_rate(10);
-
-    while (ros::ok())
-    {
-        ros::spinOnce();  
-
-        if (front_range <= 2.0)
-        {
-            msgs_cmd.linear.x = 0;
-        }
-        else
-        {
-            msgs_cmd.linear.x = 0.5;
-        }
-
-        pub_cmd_vel.publish(msgs_cmd);
-        loop_rate.sleep();  
-        ++count;
-    }
-
-    return 0;
+  ros::Rate loop_rate(30);
+  
+  while (ros::ok())
+  {
+   ROS_INFO("Sonar Range: [%f]", front_range);
+   
+   if (front_range <= 1.0)
+   {
+      msg_cmd.linear.x = 0;
+   } 
+   else
+   {
+      msg_cmd.linear.x = 0.5;
+   }
+   
+    pub_cmd_vel.publish(msg_cmd);
+  
+    ros::spinOnce();
+    ++count;
+  }
+  return 0;
 }
